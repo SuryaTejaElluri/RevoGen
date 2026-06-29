d96444d22927bf23ae2fec4d71056e9ff4814345
Surya <surya09e@gmail.com>
2026-06-29 20:39:34 +0530

    V1

--- revogen-server/src/main.ts
+++ revogen-server/src/main.ts
@@ -5,9 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:3001',
+    'https://revo-gen.vercel.app',
+  ],
+  credentials: true,
+});
 
   await app.listen(process.env.PORT ?? 3000);
 }
